a70c4a3c997fdfdb3e2379c0584a1dcb8dd855c8
slobo <145038294+Slobodenuk-Mikhail@users.noreply.github.com>
2025-12-01 21:59:53 +0300

    final commit #2

---
+++ app/src/main/java/ru/itis/android/data/DispatchersKeys.kt
@@ -0,0 +1,8 @@
+package ru.itis.android.data
+
+object DispatchersKeys {
+    const val DEFAULT = "Default"
+    const val IO = "IO"
+    const val MAIN = "Main"
+    val listOfAll = listOf(DEFAULT, IO, MAIN)
+}
